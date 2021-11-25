5c1caacfe0da16b474cfde47a186a4fd9d3a2796
Igor Leal Nodari <ilealnod@emeal.nttdata.com>
2021-11-25 18:49:27 -0300

    header pronto

--- src/structure-component/Body.js
+++ src/structure-component/Body.js
@@ -2,6 +2,11 @@ import React from 'react';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
+import perfil from "../perfil.jpg"
+import github from "../github.png"
+import email from "../email.png";
+import calendar from "../calendar.svg";
+import linkedin from "../linkedin.png";
 
 import Logo from "../shared-component/Logo";
 
@@ -10,10 +15,42 @@ function Body() {
     return (
         <body className="App-body">
             <Container>
-                <Row className="m-2">
-                   
+                <Row className="m-2 mt-4">
+
+                    <Col md={6}>
+                        <h1 className="style-hr text-title">EDUCATION</h1>
+                        <p className="text-title2">Bachelor Development And Systems Analysis</p>
+                        <p className="text-subtitle" >University Senac Santo Amaro</p>
+                        <Row>
+                            <Col md={2} className="p-0"><img src={calendar} className="App-logo-quadrado text-subtitle-gray p-2" alt="logo" /></Col>
+                            <Col md={3} className="p-0 Texto-centro"><p className="text-subtitle-gray m-0 p-0"> 2018-2020</p></Col>
+                        </Row>
+                    </Col>
+
+
+                    <Col md={6}>
+                        <h1 className="style-hr text-title marginl">FIND ME ONLINE</h1>
+                        <Col className="marginl">
+                            <Row>
+                                <Col>
+                                    <img src={github} className="App-logo p-2" alt="logo" />
+                                    <a className="p-1" href="https://github.com/Igornodari?tab=repositories"> GitHub</a>
+                                    <hr></hr>
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col>
+                                    <img src={linkedin} className="App-logo p-2" alt="logo" />
+                                    <a className="p-1" href="https://www.linkedin.com/in/igor-leal-nodari-512b7914a/"> Linkedin</a>
+                                    <hr></hr>
+                                </Col>
+                            </Row>
+                        </Col>
+
+                    </Col>
+
+
                 </Row>
-                <p></p>
             </Container>
         </body>
     )
--- src/structure-component/Header.js
+++ src/structure-component/Header.js
@@ -1,13 +1,8 @@
-import Frase from "../shared-component/Frases"
-import Logo from "../shared-component/Logo";
+
 import React from 'react'
-import logo from '../logo.svg'
 import perfil from "../perfil.jpg"
-import github from "../github.png"
 import email from "../email.png";
 import whatsapp from "../whatsapp.png";
-
-import linkedin from "../linkedin.png";
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
@@ -18,35 +13,28 @@ function Header() {
     return (
         <header className="App-header">
             <Container>
-                <Row>
-                    <Col md={2}>
+                <Row className="m-0">
+                    <Col className="App-header-itens p-2" md={2}>
                         <img src={perfil} className="App-logo-nav" alt="logo" />
                     </Col>
-                    <Col className="Texto-centro"  md={5}>
-                        <h1>IGOR LEAL NODARI</h1>
+                    <Col className="App-header-itens Texto-centro p-0" md={5}>
+                        <h1 className="text-title">IGOR LEAL NODARI</h1>
                         <p className="text-subtitle" >Front-End Developer</p>
                     </Col>
 
-                    <Col className=" App-header-itens" md={5}>
-                    <img src={github} className="App-logo p-2" alt="logo" />
-                    <a className="p-1" href="https://github.com/Igornodari?tab=repositories"> gitHub</a>
-                    <img src={email} className="App-logo p-2" alt="logo" />
-                    <a className="p-1">igor.nods@gmail.com </a>
-                    <img src={linkedin} className="App-logo p-2" alt="logo" />
-                    <a className="p-1" href="https://www.linkedin.com/in/igor-leal-nodari-512b7914a/"> linkedin</a>
-                    <img src={whatsapp} className="App-logo p-2" alt="logo" />
-                    <a className="p-1"> +55(11) 94264-7380</a>
+                    <Col className="App-header-itens Texto-centro p-0" md={5}>
+                        <Col>
+                            <img src={email} className="App-logo p-2" alt="logo" />
+                            <a className="p-1">igor.nods@gmail.com </a>
+                        </Col>
+                        <Col>
+                            <img src={whatsapp} className="App-logo p-2" alt="logo" />
+                            <a className="p-1"> +55(11) 94264-7380</a>
+                        </Col>
                     </Col>
+
                 </Row>
-
-              
-
             </Container>
-
-
-
-
-
         </header>
     )
 
