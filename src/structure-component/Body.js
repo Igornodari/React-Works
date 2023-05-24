306c03f69b0d6a7c7ff4c5a217f6c0923247402b
Igor Leal Nodari <ilealnod@emeal.nttdata.com>
2023-05-24 13:35:28 -0300

    Foi implementado Soft e tecnic skills

--- src/structure-component/Body.js
+++ src/structure-component/Body.js
@@ -15,7 +15,7 @@ function Body() {
             <Container>
                 <Row className="mt-2">
                     <Col md={6}>
-                        <h1 className="text-title">EDUCATION</h1>
+                        <h1 className="text-title m-0 p-0">EDUCATION</h1>
                         <h1 className="style-hr"></h1>
                         <p className="text-title2">Bachelor Development And Systems Analysis</p>
                         <p className="text-subtitle">University Senac Santo Amaro</p>
@@ -29,7 +29,7 @@ function Body() {
                         </Row>
 
                         <hr></hr>
-                        <h1 className="text-title">EXPERIENCE</h1>
+                        <h1 className="text-title m-0 p-0">EXPERIENCE</h1>
                         <h1 className="style-hr"></h1>
                         <p className="text-title2">FrontEnd Junior</p>
                         <p className="text-subtitle">Everis NTTDATA</p>
@@ -52,13 +52,19 @@ function Body() {
                         </Row>
                         <hr></hr>
 
-                        <h1 className="text-title">WORK GOALS</h1>
+                        <h1 className="text-title m-0 p-0">WORK GOALS</h1>
                         <h1 className="style-hr"></h1>
                         <p className="text-title2">
-                        ● Carreira em evolução na área de desenvolvimento de sistemas, com habilidade para atuar em projetos desde 2018 utilizando metodologias ágeis(SCRUM), visando a qualidade das entregas. 
-                        Sempre em foco nos testes unitarios e integrados utilizando o Bamboo para subir o codigo para esteira. Assim tendo total certeza  que o serviço prestado atinja a expectativa do cliente. </p>
+                        ● Carreira em evolução na área de desenvolvimento de sistemas, com habilidade para atuar na parte de frontend em projetos desde 2018, utilizando o framework angular8+ visando a organização, qualidade das entregas e do trabalho prestado, pronto para atuar em projetos utilizando metodologias ágeis (SCRUM). </p>
                         <h1 className="style-hr"></h1>            
-
+                        <h1 className="text-title m-0 p-0">SOFT SKILLS</h1>
+                        <h1 className="style-hr"></h1>
+                        <p className="text-title2">
+                        ● Com experiência em projetos internos na empresa e projetos nos clientes, Itau e Bradesco. 
+Tive a oportunidade de trabalhar minhas habilidades sociais, lidando com o PO, equipes de QA, Backend, Mainframe, Bff e Design de telas.
+Também durante esse tempo no meu ultimo projeto ("Bradesco"), tive a oportunidade de interagir com o cliente em atividades cotidianas como reuniões e resolvendo objetivos com eficiência.
+Evoluindo a comunicação, empatia, escuta ativa e um bom relacionamento entre ambas as partes ! </p>
+                        <h1 className="style-hr"></h1>         
                     </Col>
                     <Col md={6}>
                         <h1 className="text-title marginl">FIND ME ONLINE</h1>
@@ -128,7 +134,7 @@ function Body() {
                             </Row>
                             <hr></hr>
                         </Col>
-                        <h1 className="text-title marginl">SKILLS</h1>
+                        <h1 className="text-title marginl">TECHNICAL SKILLS</h1>
                         <Col className="marginl">
                         <h1 className="style-hr"></h1>
                             <Row>
@@ -143,6 +149,8 @@ function Body() {
                                 <p className="text-subtitle" >PostMan</p>
                                 <p className="text-subtitle" >Jira</p>
                                 <p className="text-subtitle" >Sass</p>
+                                <p className="text-subtitle" >Bootstrap</p>
+                                <p className="text-subtitle" >Ionic</p>
                                 </Col>
                                 <Col>
                                 <p className="text-subtitle" >React</p>
@@ -155,6 +163,8 @@ function Body() {
                                 <p className="text-subtitle" >TypeScript</p>
                                 <p className="text-subtitle" >Jasmine</p>
                                 <p className="text-subtitle" >Bamboo</p>
+                                <p className="text-subtitle" >Angular Material</p>
+                                <p className="text-subtitle" >FlexBox</p>
                                 </Col>
                             </Row>
                             <hr></hr>
